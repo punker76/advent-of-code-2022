74ad1785fffceff1f66d5d6f0e7f616960de3206
punker76 <punkerat76@gmail.com>
2022-12-01 10:10:53 +0100

    feat: solve day 1 (part 1 and 2)

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -2,16 +2,51 @@ import Puzzle from '../../types/AbstractPuzzle';
 
 export default class ConcretePuzzle extends Puzzle {
   public solveFirst(): string {
-    return 'day 1 solution 1';
+    const calories = this.input
+      .split('\n')
+      .map((x) => (x && x !== '' ? +x : -1));
+
+    let maxTotalCalories = 0;
+    let curentCal = 0;
+
+    calories.forEach((cal) => {
+      if (cal >= 0) {
+        curentCal += cal;
+        maxTotalCalories = Math.max(maxTotalCalories, curentCal);
+      } else {
+        curentCal = 0;
+      }
+    });
+
+    return 'day 1 solution 1: ' + maxTotalCalories;
   }
   public solveSecond(): string {
-    return 'day 1 solution 2';
+    const calories = this.input
+      .split('\n')
+      .map((x) => (x && x !== '' ? +x : -1));
+
+    let currentElf = 0;
+    let elvesCalories: number[] = [];
+
+    calories.forEach((cal) => {
+      if (cal >= 0) {
+        elvesCalories[currentElf] = (elvesCalories[currentElf] ?? 0) + cal;
+      } else {
+        currentElf++;
+      }
+    });
+
+    elvesCalories = elvesCalories.sort((a, b) => b - a);
+    return (
+      'day 1 solution 2: ' +
+      (elvesCalories[0] + elvesCalories[1] + elvesCalories[2])
+    );
   }
 
   public getFirstExpectedResult(): string {
-    return 'day 1 solution 1';
+    return 'day 1 solution 1: xxxxx';
   }
   public getSecondExpectedResult(): string {
-    return 'day 1 solution 2';
+    return 'day 1 solution 2: xxxxxx';
   }
 }
