631c1dad80c15b521b157c59021881fe6353ce0e
punker76 <punkerat76@gmail.com>
2022-12-04 14:26:50 +0100

    feat: solve day 2 part 2

--- src/days/2/Puzzle.ts
+++ src/days/2/Puzzle.ts
@@ -60,13 +60,67 @@ export default class ConcretePuzzle extends Puzzle {
     return 'day 2 solution 1: 10816';
   }
 
+  private getMyResultFor2(
+    player1: { RPC: string },
+    player2: { RPC: string }
+  ): number {
+    // console.log(player1, player2);
+
+    // Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock.
+
+    // X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
+
+    if (player1.RPC === 'A') {
+      if (player2.RPC === 'X') {
+        return 3 + 0;
+      } else if (player2.RPC === 'Y') {
+        return 1 + 3;
+      } else if (player2.RPC === 'Z') {
+        return 2 + 6;
+      }
+    } else if (player1.RPC === 'B') {
+      if (player2.RPC === 'X') {
+        return 1 + 0;
+      } else if (player2.RPC === 'Y') {
+        return 2 + 3;
+      } else if (player2.RPC === 'Z') {
+        return 3 + 6;
+      }
+    } else if (player1.RPC === 'C') {
+      if (player2.RPC === 'X') {
+        return 2 + 0;
+      } else if (player2.RPC === 'Y') {
+        return 3 + 3;
+      } else if (player2.RPC === 'Z') {
+        return 1 + 6;
+      }
+    }
+
+    return 0;
+  }
+
   public solveSecond(): string {
     // WRITE SOLUTION FOR TEST 2
-    return 'day 2 solution 2';
+    const strategyGuide = this.input.trim().split('\n');
+    const result = strategyGuide
+      .map((l) => {
+        const line = [...l];
+        // console.log(line);
+        const myResult = this.getMyResultFor2(
+          {
+            RPC: line[0],
+          },
+          { RPC: line[2] }
+        );
+        // console.log(myResult);
+        return myResult;
+      })
+      .reduce((p, c) => p + c);
+    return `day 2 solution 2: ${result}`;
   }
 
   public getSecondExpectedResult(): string {
     // RETURN EXPECTED SOLUTION FOR TEST 2;
-    return 'day 2 solution 2';
+    return 'day 2 solution 2: 11657';
   }
 }
