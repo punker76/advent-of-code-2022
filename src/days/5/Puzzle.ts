1bbc9b74735ca5de01b392becd24fb9cbdb6e071
punker76 <punkerat76@gmail.com>
2022-12-06 19:19:20 +0100

    feat: solve day 5 part 2

--- src/days/5/Puzzle.ts
+++ src/days/5/Puzzle.ts
@@ -64,11 +64,65 @@ export default class ConcretePuzzle extends Puzzle {
 
   public solveSecond(): string {
     // WRITE SOLUTION FOR TEST 2
-    return 'day 5 solution 2';
+    const [stacksInput, movesInput] = this.input.split('\n\n');
+
+    const stacks = stacksInput.split('\n');
+    const moves = movesInput.trim().split('\n');
+
+    // console.log('Stacks', stacks);
+    // console.log('Moves', moves);
+
+    const stackMatrix: string[][] = [];
+
+    [...stacks[stacks.length - 1]].forEach((s, i) => {
+      if (+s) {
+        // console.log(s, i);
+        stackMatrix.push([]);
+
+        for (let index = stacks.length - 2; index >= 0; index--) {
+          const element = [...stacks[index]][i];
+          if (element != ' ') {
+            // console.log('line', index, element);
+            stackMatrix[+s - 1].push(element);
+          }
+        }
+      }
+    });
+
+    // console.log(stackMatrix);
+
+    moves.forEach((m) => {
+      // console.log(m);
+
+      const moveRegEx = new RegExp(
+        'move (?<count>\\d+) from (?<from>\\d+) to (?<to>\\d+)',
+        'g'
+      );
+
+      const moveInstruction = moveRegEx.exec(m);
+      const count = +moveInstruction?.groups?.count;
+      const from = +moveInstruction?.groups?.from;
+      const to = +moveInstruction?.groups?.to;
+
+      // console.log(count, from, to);
+
+      const crates = stackMatrix[from - 1].splice(
+        stackMatrix[from - 1].length - count,
+        count
+      );
+
+      stackMatrix[to - 1].splice(stackMatrix[to - 1].length, 0, ...crates);
+    });
+
+    // console.log(stackMatrix);
+
+    const result = stackMatrix.map((s) => s.pop()).reduce((p, c) => p + c);
+
+    return `day 5 solution 2: ${result}`;
   }
 
   public getSecondExpectedResult(): string {
     // RETURN EXPECTED SOLUTION FOR TEST 2;
-    return 'day 5 solution 2';
+    return 'day 5 solution 2: GGNPJBTTR';
   }
 }
