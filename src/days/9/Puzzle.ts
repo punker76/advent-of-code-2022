785ae8aabfd587340cf85725fa7076593b2c8cc4
punker76 <punkerat76@gmail.com>
2022-12-12 10:13:16 +0100

    feat: solve day 9 part 2

--- src/days/9/Puzzle.ts
+++ src/days/9/Puzzle.ts
@@ -1,5 +1,10 @@
 import Puzzle from '../../types/AbstractPuzzle';
 
+interface Point {
+  x: number;
+  y: number;
+}
+
 export default class ConcretePuzzle extends Puzzle {
   public solveFirst(): string {
     // WRITE SOLUTION FOR TEST 1
@@ -57,11 +62,65 @@ export default class ConcretePuzzle extends Puzzle {
 
   public solveSecond(): string {
     // WRITE SOLUTION FOR TEST 2
-    return 'day 9 solution 2';
+    const motions = this.input.trim().split('\n');
+    // console.log(motions);
+    const points: Point[] = [
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+      { x: 0, y: 0 },
+    ];
+    const result = new Set<string>();
+    motions
+      .map((m) => {
+        const [dir, count] = m.split(' ');
+        return { dir, count: +count };
+      })
+      .forEach((inst) => {
+        for (let index = 0; index < inst.count; index++) {
+          if (inst.dir === 'L') {
+            points[0].x--;
+          } else if (inst.dir === 'R') {
+            points[0].x++;
+          } else if (inst.dir === 'U') {
+            points[0].y--;
+          } else if (inst.dir === 'D') {
+            points[0].y++;
+          }
+
+          points.forEach((p, i, a) => {
+            if (i > 0) {
+              const diff = {
+                x: a[i - 1].x - a[i].x,
+                y: a[i - 1].y - a[i].y,
+              };
+
+              if (Math.abs(diff.x) > 1 || Math.abs(diff.y) > 1) {
+                a[i].x += Math.sign(diff.x);
+                a[i].y += Math.sign(diff.y);
+              }
+            }
+          });
+
+          // result.add(`${tx},${ty}`);
+          const t = points[points.length - 1];
+          // console.log(`inst.dir: ${inst.dir} tx: ${t.x} ty: ${t.y}`);
+          // console.log(points);
+          result.add(`${t.x},${t.y}`);
+        }
+      });
+    // console.log(result);
+    return `day 9 solution 2: ${result.size}`;
   }
 
   public getSecondExpectedResult(): string {
     // RETURN EXPECTED SOLUTION FOR TEST 2;
-    return 'day 9 solution 2';
+    return 'day 9 solution 2: 2352';
   }
 }
