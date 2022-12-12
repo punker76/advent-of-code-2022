0deeda40b26807353a1b7a0ff58f68ef62afeee1
punker76 <punkerat76@gmail.com>
2022-12-12 12:44:50 +0100

    feat: solve day 10 part 2

--- src/days/10/Puzzle.ts
+++ src/days/10/Puzzle.ts
@@ -51,11 +51,53 @@ export default class ConcretePuzzle extends Puzzle {
 
   public solveSecond(): string {
     // WRITE SOLUTION FOR TEST 2
-    return 'day 10 solution 2';
+    const program = this.input.trim().split('\n');
+    // console.log(program);
+
+    const register = { X: 1, cycle: 0 };
+    const crt: string[] = [];
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const [instruction, value] = program.splice(0, 1).pop().split(' ');
+      const instructionValue = value ? +value : 0;
+      const cycle = instruction === 'addx' ? 2 : 1;
+
+      for (let index = 0; index < cycle; index++) {
+        const rowPos = register.cycle % 40;
+        if ([register.X - 1, register.X, register.X + 1].includes(rowPos)) {
+          crt.push('#');
+        } else {
+          crt.push('.');
+        }
+
+        register.cycle++;
+        if (index === cycle - 1) {
+          register.X += instructionValue;
+        }
+      }
+
+      if (!program.length) {
+        break;
+      }
+    }
+
+    const result = [
+      crt.splice(0, 40).join(''),
+      crt.splice(0, 40).join(''),
+      crt.splice(0, 40).join(''),
+      crt.splice(0, 40).join(''),
+      crt.splice(0, 40).join(''),
+      crt.splice(0, 40).join(''),
+    ];
+
+    console.log(result.join('\n'));
+
+    return 'day 10 solution 2: EGLHBLFJ';
   }
 
   public getSecondExpectedResult(): string {
     // RETURN EXPECTED SOLUTION FOR TEST 2;
-    return 'day 10 solution 2';
+    return 'day 10 solution 2: EGLHBLFJ';
   }
 }
